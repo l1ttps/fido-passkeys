82256a2852d4be158d68f97456a5f92348dc5f72
l1ttps <l1ttps443@gmail.com>
2023-09-30 17:00:34 +0700

    feat: config tailwind

--- .client/vite.config.ts
+++ .client/vite.config.ts
@@ -1,10 +1,19 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+  ],
   build: {
-    outDir: '../public',
+    outDir: "../public",
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
   },
 });
