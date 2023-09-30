ef50c33ebd1f44f7458fb47f5605073e3abb229b
l1ttps <32523515+l1ttps@users.noreply.github.com>
2023-09-30 05:50:17 +0000

    feat: elysia render vite + react

---
+++ src/comps/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            hello
+        </div>
+    );
+};
+
+export default Test;
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { html } from '@elysiajs/html';
 import staticPlugin from "@elysiajs/static";
 import { Elysia } from "elysia";
 import rootControler from './controllers/root.controller';
-new Elysia()
-  .use(staticPlugin())
+const app = new Elysia()
+  .use(staticPlugin({
+    prefix: "/"
+  }))
   .use(html())
+
   .use(rootControler())
     .listen(3000)
+
+  console.log(`🚀 Server running on ${app.server?.port}`);
