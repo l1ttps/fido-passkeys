bedfee49225bac5904543a9a5422d68f1bf44294
l1ttps <l1ttps443@gmail.com>
2023-10-01 22:18:32 +0700

    feat(client): setup react-query

--- .client/src/App.tsx
+++ .client/src/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./page/login";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Fido pass key</h1>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Fido pass key</h1>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 export default App;
